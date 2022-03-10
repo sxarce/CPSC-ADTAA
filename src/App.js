e0faeecfd8e57bbe7fe449e7e22a80da917c2657
MitchelCarreon <60196056+MitchelCarreon@users.noreply.github.com>
2022-03-10 17:45:57 -0600

    Working on registration page

--- src/App.js
+++ src/App.js
@@ -7,14 +7,22 @@ import {
   useContext,
   useRef,
 } from "react";
-import LoginPage from './pages/LoginPage/LoginPage'
-import DashboardPage from './pages/DashboardPage/DashboardPage'
-import { BrowserRouter as Router, Routes, Switch, Route, useRoutes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import DashboardPage from "./pages/DashboardPage/DashboardPage";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Switch,
+  Route,
+  useRoutes,
+} from "react-router-dom";
+import RegisterPage from "./pages/RegistrationPage/RegisterPage";
 
 const App = () => {
   let routes = useRoutes([
     { path: "/", element: <LoginPage /> },
     { path: "/login", element: <LoginPage /> },
+    { path: "/register", element: <RegisterPage /> },
   ]);
   return routes;
 };
---
+++ src/pages/RegistrationPage/RegisterPage.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./RegisterPage.css";
+import rect_left from "../../assets/svg/background_rectangle.svg";
+import rect_right from "../../assets/svg/background_rectangle2.svg";
+import ualrLogo from "../../assets/svg/uarLogoRed.svg"
+
+export default function RegisterPage() {
+  return (
+    <div className="background-register">
+      <img
+        className="red-rectangle-left"
+        src={rect_left}
+        alt="left red rectangle"
+      />
+      <img
+        className="red-rectangle-right"
+        src={rect_right}
+        alt="right red rectangle"
+      />
+
+      <img className="ualr-logo-red" src={ualrLogo} />
+
+      <div className="card">
+        <section className="card-title">
+          <h1 className="card-title-create">Create an account</h1>
+          <p>Submit a registration request with your desired access level</p>
+        </section>
+
+        <form className="input-fields">
+          <input
+            type="text"
+            className="input-field-email"
+            placeholder="Email Address"
+          />
+
+          <input type="text" placeholder="Username" />
+          <input type="text" placeholder="Password" />
+          <input type="text" placeholder="Confirm password" />
+          <button className="register-btn">Create Account</button>
+
+          <div>
+            <span>Already Have An Account? </span>
+            <span className="sign-in-text">Sign In</span>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
