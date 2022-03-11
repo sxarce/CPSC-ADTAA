d0ff9a10e15389cd638cf395173a0e627ed4d018
MitchelCarreon <60196056+MitchelCarreon@users.noreply.github.com>
2022-03-10 20:21:54 -0600

    registration page looks funky in small screens

--- src/pages/RegistrationPage/RegisterPage.js
+++ src/pages/RegistrationPage/RegisterPage.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./RegisterPage.css";
 import rect_left from "../../assets/svg/background_rectangle.svg";
 import rect_right from "../../assets/svg/background_rectangle2.svg";
-import ualrLogo from "../../assets/svg/uarLogoRed.svg"
+import ualrLogo from "../../assets/svg/uarLogoRed.svg";
 
 export default function RegisterPage() {
   return (
@@ -18,7 +18,7 @@ export default function RegisterPage() {
         alt="right red rectangle"
       />
 
-      <img className="ualr-logo-red" src={ualrLogo} />
+      <img className="ualr-logo-red" src={ualrLogo} alt="ualr logo in red"/>
 
       <div className="card">
         <section className="card-title">
@@ -26,21 +26,22 @@ export default function RegisterPage() {
           <p>Submit a registration request with your desired access level</p>
         </section>
 
+        <div> </div>
         <form className="input-fields">
-          <input
-            type="text"
-            className="input-field-email"
-            placeholder="Email Address"
-          />
-
-          
+          <div className="input-fields-top">
+            <input
+              type="text"
+              className="input-field-email"
+              placeholder="Email Address"
+            />
+          </div>
 
           <input type="text" placeholder="Username" />
           <input type="text" placeholder="Password" />
           <input type="text" placeholder="Confirm password" />
           <button className="register-btn">Create Account</button>
 
-          <div>
+          <div className="sign-in-full-text"> 
             <span>Already Have An Account? </span>
             <span className="sign-in-text">Sign In</span>
           </div>
