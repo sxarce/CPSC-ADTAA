25eddbe44d71750c99e979b22d2aadd2eaace28b
MitchelCarreon <60196056+MitchelCarreon@users.noreply.github.com>
2022-03-14 22:37:28 -0500

    Added MUI Paginated table

--- src/components/InstructorSetupTable.js
+++ src/components/InstructorSetupTable.js
@@ -1,50 +1,274 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableHead from "@mui/material/TableHead";
+import TableContainer from "@mui/material/TableContainer";
+import TableFooter from "@mui/material/TableFooter";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+import IconButton from "@mui/material/IconButton";
+import FirstPageIcon from "@mui/icons-material/FirstPage";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import LastPageIcon from "@mui/icons-material/LastPage";
+
+import AddIcon from "@mui/icons-material/Add";
+import { Button } from "@mui/material";
+
 import "./InstructorSetupTable.css";
-import React from "react";
-import plusIcon from "../assets/svg/plus-icon.svg";
+import { ThemeProvider, createTheme, styled } from "@mui/material/styles";
 
-export default function InstructorSetupTable() {
+function TablePaginationActions(props) {
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
+
+  const handleFirstPageButtonClick = (event) => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
   return (
-    <table className="roster-table">
-      <thead>
-        <tr className="table-title">
-          <th colSpan={3} style={{ fontWeight: "normal" }}>
-            Current Roster
-          </th>
-          <th style={{ textAlign: "end", paddingRight: "0.75rem" }}>
-            Instructors
-          </th>
-        </tr>
-        <tr className="table-headers">
-          <th>Last Name</th>
-          <th>First Name</th>
-          <th>Expertise</th>
-          <th>Maximum Load</th>
-        </tr>
-      </thead>
-
-      <tbody>
-        <tr className="table-data">
-          <td>Doe</td>
-          <td>John</td>
-          <td>
-            <ul>
-              <li> Software Development</li>
-              <li> Cryptography</li>
-            </ul>
-          </td>
-          <td>60%</td>
-        </tr>
-      </tbody>
-
-      <tfoot>
-        <tr>
-          <td colSpan={4}>
-            <button className="add-instructor-btn">
-              <img src={plusIcon} alt="plus" />
-            </button>
-          </td>
-        </tr>
-      </tfoot>
-    </table>
+    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowRight />
+        ) : (
+          <KeyboardArrowLeft />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowLeft />
+        ) : (
+          <KeyboardArrowRight />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </Box>
   );
+}
+
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
+
+function createData(name, calories, fat) {
+  return { name, calories, fat };
+}
+
+const rows = [
+  createData("Cupcake", 305, 3.7),
+  createData("Donut", 452, 25.0),
+  createData("Eclair", 262, 16.0),
+  createData("Frozen yoghurt", 159, 6.0),
+  createData("Gingerbread", 356, 16.0),
+  createData("Honeycomb", 408, 3.2),
+  createData("Ice cream sandwich", 237, 9.0),
+  createData("Jelly Bean", 375, 0.0),
+  createData("KitKat", 518, 26.0),
+  createData("Lollipop", 392, 0.2),
+  createData("Marshmallow", 318, 0),
+  createData("Nougat", 360, 19.0),
+  createData("Oreo", 437, 18.0),
+].sort((a, b) => (a.calories < b.calories ? -1 : 1));
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      "Open sans"
+    ]
+  },
+ 
+  
+})
+
+export default function CustomPaginationActionsTable() {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const HeaderStyle = {
+    fontWeight: "bold",
+    borderBottom: "none",
+    // fontFamily: "Open Sans",
+    color: "#8898AA",
+  };
+  const HeaderBackgroundStyle = {
+    backgroundColor: "#F6F9FC",
+    borderBottom: "1px solid #E9ECEF",
+    borderTop: "1px solid #E9ECEF",
+  };
+
+  return (
+    <TableContainer component={Paper} style={{ width: "60rem" }}>
+      <ThemeProvider theme={theme}>
+        <Table sx={{ minWidth: 500 }} aria-label="Instructor table">
+          <TableHead >
+            <TableRow >
+              <TableCell
+                colSpan={3}
+                style={{ fontWeight: "bold", fontSize: "0.9rem", padding: "1rem 1rem 1.3rem 1rem" }}
+              >
+                Current Roster
+              </TableCell>
+              <TableCell align="right" >
+                Instructors
+              </TableCell>
+            </TableRow>
+            <TableRow style={HeaderBackgroundStyle}>
+              <TableCell style={HeaderStyle}>Last Name</TableCell>
+              <TableCell style={HeaderStyle}>First Name</TableCell>
+              <TableCell style={HeaderStyle}>Expertise</TableCell>
+              <TableCell style={HeaderStyle}>Maximum load</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {(rowsPerPage > 0
+              ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              : rows
+            ).map((row) => (
+              <TableRow key={row.name}>
+                <TableCell component="th" scope="row" style={{fontSize: "small"}}>
+                  {row.name}
+                </TableCell>
+                <TableCell style={{ width: 160 }} align="left" style={{fontSize: "small"}}>
+                  {row.calories}
+                </TableCell>
+                <TableCell style={{ width: 160 }} align="left" style={{fontSize: "small"}}>
+                  {row.fat}
+                </TableCell>
+              </TableRow>
+            ))}
+
+            {emptyRows > 0 && (
+              <TableRow style={{ height: 53 * emptyRows }}>
+                <TableCell colSpan={6} />
+              </TableRow>
+            )}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <Button className="add-instructor-btn" variant="contained">
+                <AddIcon />
+              </Button>
+              <TablePagination
+                rowsPerPageOptions={[5, 10, { label: "All", value: -1 }]}
+                colSpan={3}
+                count={rows.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                SelectProps={{
+                  inputProps: {
+                    "aria-label": "Rows per page",
+                  },
+                  native: true,
+                }}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+                ActionsComponent={TablePaginationActions}
+              />
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </ThemeProvider>
+    </TableContainer>
+  );
+}
+
+{
+  /* <table className="roster-table">
+<thead>
+  <tr className="table-title">
+    <th colSpan={3} style={{ fontWeight: "normal" }}>
+      Current Roster
+    </th>
+    <th style={{ textAlign: "end", paddingRight: "0.75rem" }}>
+      Instructors
+    </th>
+  </tr>
+  <tr className="table-headers">
+    <th>Last Name</th>
+    <th>First Name</th>
+    <th>Expertise</th>
+    <th>Maximum Load</th>
+  </tr>
+</thead>
+
+<tbody>
+  <tr className="table-data">
+    <td>Doe</td>
+    <td>John</td>
+    <td>
+      <ul>
+        <li> Software Development</li>
+        <li> Cryptography</li>
+      </ul>
+    </td>
+    <td>60%</td>
+  </tr>
+</tbody>
+
+<tfoot>
+  <tr>
+    <td colSpan={4}>
+      <button className="add-instructor-btn">
+        <img src={plusIcon} alt="plus" />
+      </button>
+    </td>
+  </tr>
+</tfoot>
+</table> */
 }
--- src/pages/SetupPage/SetupPage.js
+++ src/pages/SetupPage/SetupPage.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./SetupPage.css";
-import Sidebar from "../../components/Sidebar"
-import InstructorSetupTable from "../../components/InstructorSetupTable"
+import Sidebar from "../../components/Sidebar";
+import InstructorSetupTable from "../../components/InstructorSetupTable";
+import CourseSetupTable from "../../components/CourseSetupTable"
 import gearBackground from "../../assets/svg/background_gear.svg";
+
 
 export default function SetupPage() {
   return (
@@ -10,14 +12,13 @@ export default function SetupPage() {
       <div className="banner">
         <img src={gearBackground} alt="gear logo" className="gear-background" />
       </div>
-      
-      <Sidebar page="setup"/>
+
+      <Sidebar page="setup" />
 
       <div className="table-container">
         <InstructorSetupTable />
-        
+       
       </div>
-
 
       
     </div>
